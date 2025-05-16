8a4307eb368dab33f481dae13e6c0051f9dccae8
Artyom <goglakk2@gmail.com>
2025-05-16 17:51:11 +0300

    Listeners

--- app/src/main/java/com/example/remotecontrol/MainActivity.kt
+++ app/src/main/java/com/example/remotecontrol/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.example.remotecontrol
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
 import com.example.remotecontrol.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +14,28 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
+        }
+
+        val numberBtnListener = OnClickListener { view ->
+            val text = (view as AppCompatButton).text
+            with(binding) {
+                if (workingText.text.equals("0")) {
+                    workingText.text = text
+                } else {
+                    workingText.text = "${workingText.text} ${text}"
+                }
+            }
+        }
+
+        with(binding){
+            controlZeroBtn.setOnClickListener(numberBtnListener)
+            controlOneBtn.setOnClickListener(numberBtnListener)
+            controlEnterBtn.setOnClickListener {
+                if(workingText.text.isNotEmpty()){
+                    selectedText.text = workingText.text
+                }
+                workingText.text = "0"
+            }
         }
 
     }
